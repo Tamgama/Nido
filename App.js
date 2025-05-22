89b81d1cfe3a8779e9afed7064cf383fa633f7c5
Tamgama <tamara.garmart@gmail.com>
2025-05-22 17:04:53 +0200

    UPD - playing with some buttons and installing eslint y prettier

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight, TouchableOpacity} from 'react-native';
+
+const icon = require('./assets/otter-logo.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={icon} style={{width: 150, height: 150}}/>
+      <StatusBar style="auto"/>
+
+      <Text>Nido</Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Bienvenide 💛')}
+        style={{
+          width: 200,
+          height: 50,
+          backgroundColor: 'orange',
+          borderColor: 'black',
+          borderRadius:20,
+          justifyContent: 'center',
+          alignItems: 'center'}}
+        >
+        <Text style={{color: 'white',
+          textAlign: 'center',
+          fontWeight: 'bold', 
+}}
+          > Gracias por venir </Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
